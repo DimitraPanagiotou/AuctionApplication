3faffb7c745ab64ff7a19b55873421e20f9321f6
dimpanagiotou <panagiotoudi@gmail.com>
2021-05-23 15:39:36 +0300

    Updates

---
+++ precache-manifest.0197594b9f65423c88b798831049f68d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a804f1122477aebf5f84c8168248db7",
+    "url": "/AuctionApplication/index.html"
+  },
+  {
+    "revision": "fc4e1554644c33693f2d",
+    "url": "/AuctionApplication/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8bc513df4b3b18df4e05",
+    "url": "/AuctionApplication/static/js/2.62439e57.chunk.js"
+  },
+  {
+    "revision": "fc4e1554644c33693f2d",
+    "url": "/AuctionApplication/static/js/main.669f8b8b.chunk.js"
+  },
+  {
+    "revision": "dec0576b6ccf1278c875",
+    "url": "/AuctionApplication/static/js/runtime-main.cce14543.js"
+  }
+]);
